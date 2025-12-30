95efdda4eed8b1723d33dd92f2d778ace73a757d
Fariduddin khan <fariduddinkhan7860@gmail.com>
2025-12-30 07:40:06 +0530

    fixed auth context

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -7,7 +7,7 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    // 🔥 Restore login on refresh
+    //Restore login on refresh
     useEffect(() => {
         const token = localStorage.getItem("token");
 
